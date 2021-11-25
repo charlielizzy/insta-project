8006416af26258d2d5d9b703706e65c27f8177dc
Charlotte Walker <charlielizzy@me.com>
2021-11-25 18:41:42 +0000

    create card

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-fetch(/data/posts.json).then((response)=>{
-    return response.jason()})
-}).then((data)=>{
+fetch("../data/posts.json")
+.then(response => {
+    return response.json
+})
+.then(response=>{
     createPosts(data)
 })
+
+const createPosts = (posts) =>{
+forEach((post)=>{
+//create card in post
+const card = document.createElement('div');
+card.classList.add('card');
+container.appendChild(card)
+})
+}
