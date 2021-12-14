e95720af49187dc0685c1ab456ddc41e200dd708
Charlotte Walker <charlielizzy@me.com>
2021-12-14 20:11:12 +0000

    new cards

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
+const container = document.querySelector('#container');
+
 fetch("../data/posts.json")
-.then(response => {
-    return response.json
+.then((response) => {
+    return response.json();
 })
-.then(response=>{
+.then((data) => {
     createPosts(data)
-})
+});
 
 const createPosts = (posts) =>{
-forEach((post)=>{
+posts.forEach((post)=>{
+
 //create card in post
 const card = document.createElement('div');
 card.classList.add('card');
-container.appendChild(card)
+container.appendChild(card);
+
+//create image in card
+const image = document.createElement('img');
+image.classList.add('picture');
+image.src = post.image_url;
+card.appendChild(image);
+
+//create content in card
+const content = document.createElement('div');
+content.classList.add('content');
+card.appendChild(content);
+
+//create like icon in content
+const likeIcon = document.createElement('i');
+likeIcon.classList.add('far fa-heart fa-2x like-button');
+content.appendChild(likeIcon);
+
+//create owner in content
+const owner = document.createElement('h3');
+owner.classList.add('owner');
+owner.innerText = post.owner;
+content.appendChild(owner);
+
+//create description in content
+const description = document.createElement('p');
+description.classList.add('description');
+description.innerText = post.description;
+content.appendChild(description);
+
+
+
 })
 }
